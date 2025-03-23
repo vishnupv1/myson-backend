a745542fe9d6b960f7d96af2804fdfd2c7132213
vishnupvnhrt <vishnu@nhrtechnologies.com>
2025-03-23 22:49:23 +0530

    env

--- config/db.js
+++ config/db.js
@@ -1,14 +1,14 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/productDB', {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log('MongoDB connected successfully');
+    console.log("MongoDB connected successfully");
   } catch (err) {
-    console.error('Error connecting to MongoDB:', err.message);
+    console.error("Error connecting to MongoDB:", err.message);
     process.exit(1);
   }
 };
--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 // MongoDB connection
-mongoose.connect("mongodb://localhost:27017/myson", {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
