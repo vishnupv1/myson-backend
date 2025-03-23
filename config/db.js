67c5dbea405acb729726e087e4454ca6817ffc0c
vishnupvnhrt <vishnu@nhrtechnologies.com>
2025-03-23 22:54:40 +0530

    consoles

--- config/db.js
+++ config/db.js
@@ -2,8 +2,6 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    console.log(process.env.MONGO_URI,"envwwwwwwwwwwwwwwwwwwwwwwwwwwwww");
-    
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
--- server.js
+++ server.js
@@ -13,7 +13,6 @@ app.use(cors());
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 // MongoDB connection
-console.log(process.env.MONGO_URI, "envwwwwwwwwwwwwwwwwwwwwwwwwwwwww");
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
